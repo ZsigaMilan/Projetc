f05fa83aebd155bc56b17b53a7ea1511bd3e6bab
Milan <zsigamilan05@gmail.com>
2020-02-17 09:28:23 +0100

    prog

--- githubproject/Program.cs
+++ githubproject/Program.cs
@@ -6,7 +6,7 @@ namespace githubproject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
         }
     }
 }
---
+++ githubproject/auto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace githubproject
+{
+    class auto
+    {
+        private string marka;
+        private string modellnev;
+        private int evjarat;
+        private int kilometeroraallas;
+        private string uzemanyagtipus;
+        private int ar;
+
+   (StreamReader sr = new StreamReader("auto.csv"))
+    {
+
+
+    }
+    
+   
+        
+    }
+
+{
