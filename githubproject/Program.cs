21cea2cd9047d1ddc51a119f221e6691bcae55d5
Milan <zsigamilan05@gmail.com>
2020-02-17 08:31:16 +0100

    Add project files.

---
+++ githubproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
